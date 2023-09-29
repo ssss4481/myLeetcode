9fa317eef389325ca407c33651eff43a9a846671
Andrew Chen <ssss4480@gmail.com>
2023-09-29 14:48:13 +0800

    20230929

---
+++ 896_Monotonic_Array.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+static const int fast_io = []()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
+class Solution 
+{
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        const int n = nums.size();
+        
+        if(n == 1)
+        {
+            return true;
+        }
+
+        auto check = [&](auto compare)
+        {
+            for(int i = 1; i < n; ++i)
+            {
+                if(compare(nums[i-1], nums[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        if(nums.front() < nums.back())
+        {
+            return check([](int& a, int& b){return a > b;});
+        }
+        else if(nums.front() == nums.back())
+        {
+            return check([](int& a, int& b){return a != b;});
+        }
+        else
+        {
+            return check([](int& a, int& b){return a < b;});
+        }
+    }
+};
---
+++ fast_io.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+static const int fast_io = []()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
