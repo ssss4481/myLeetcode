744a3b9aadccd08fc9144cec5bb4134bf9a133a8
Andrew Chen <ssss4480@gmail.com>
2024-01-17 13:07:45 +0800

    before 20240116

--- 2385_Amount_of_Time_for_Binary_Tree_to_Be_Infected.cpp
+++ 2385_Amount_of_Time_for_Binary_Tree_to_Be_Infected.cpp
@@ -2,6 +2,14 @@
 #include <algorithm>
 #include "TreeNode.hpp"
 
+static const int fast_io = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return 0;
+}();
+
 class Solution 
 {
 public:
