8aa02a1f1910e0c63a632a06caa36d986f3f3928
Andrew Chen <ssss4480@gmail.com>
2024-02-19 13:24:43 +0800

    20240219

---
+++ 231_Power_of_Two.cpp
@@ -0,0 +1,14 @@
+#include <climits>
+class Solution 
+{
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        if(n <= 0)
+        {
+            return false;
+        }
+
+        return (n & (n-1)) == 0;        
+    }
+};
